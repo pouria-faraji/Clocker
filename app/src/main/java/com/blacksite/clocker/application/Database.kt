65357d6aee58d87f4676905dd8e073c96282659a
Pouria Faraji <pouria.faraji@gmail.com>
2018-04-30 15:27:20 +0430

    grid

--- app/src/main/java/com/blacksite/clocker/application/Database.kt
+++ app/src/main/java/com/blacksite/clocker/application/Database.kt
@@ -21,8 +21,15 @@ class Database {
         faceList.add(Face(R.drawable.skull_512, R.drawable.skull_512))
         faceList.add(Face(R.drawable.jellyfish_512, R.drawable.jellyfish_512))
         faceList.add(Face(R.drawable.lion_512, R.drawable.lion_512_w))
+        faceList.add(Face(R.drawable.flower_2_512, R.drawable.flower_2_512_w))
         faceList.add(Face(R.drawable.tiger_512, R.drawable.tiger_512_w))
+        faceList.add(Face(R.drawable.flower_3_512, R.drawable.flower_3_512_w))
         faceList.add(Face(R.drawable.panther_512, R.drawable.panther_512_w))
+        faceList.add(Face(R.drawable.flower_1_512, R.drawable.flower_1_512_w))
+        faceList.add(Face(R.drawable.girl_1_512, R.drawable.girl_1_512_w))
+        faceList.add(Face(R.drawable.flower_4_512, R.drawable.flower_4_512_w))
+        faceList.add(Face(R.drawable.cactus_1_512, R.drawable.cactus_1_512_w))
+        faceList.add(Face(R.drawable.cactus_2_512, R.drawable.cactus_2_512_w))
         for (face in faceList){
             face.insert()
         }
@@ -32,6 +39,7 @@ class Database {
         dialList.add(Dial(R.drawable.no_dial))
         dialList.add(Dial(R.drawable.dial_1))
         dialList.add(Dial(R.drawable.dial_2))
+        dialList.add(Dial(R.drawable.dial_3))
         for (dial in dialList){
             dial.insert()
         }
